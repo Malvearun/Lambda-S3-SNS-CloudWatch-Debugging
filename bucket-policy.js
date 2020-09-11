a6344db57b47c119fef2b6cf30f06fec3d9495d0
Malvearun <37962813+Malvearun@users.noreply.github.com>
2020-09-11 12:35:27 +0100

    Create bucket-policy.js

---
+++ bucket-policy.js
@@ -0,0 +1,27 @@
+{
+  "Version":"2012-10-17",
+  "Statement": [
+    {
+        "Effect": "Allow",
+        "Action": "logs:CreateLogGroup",
+        "Resource": "arn:aws:logs::eu-west-2:108.....:*"
+     },
+    {
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogStream",
+               "logs:PutLogEvents"
+        ],
+        "Resource": [
+            "arn:aws:logs::eu-west-2:108.....:log-group:.aws/lambda/S3Trigger*"
+    },
+
+    {
+        "Effect": "Allow",
+        "Action": [
+            "SNS:Publish"
+        ],
+        "Resource": "*",
+     }
+  ]
+}
